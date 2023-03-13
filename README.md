0028cbffae9eb0b716e19b5dc8281e02f3c51474
Alexandre Gomes <119986066+alefgomes0@users.noreply.github.com>
2023-03-13 12:30:09 -0300

    Add files and calculator background on HTML

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Calculator
+Simple calculator made with HTML, CSS and vanilla JavaScript as a project for TheOdinProject curriculum.
---
+++ calculator.js
@@ -0,0 +1,18 @@
+function sum(a, b) {
+  return a + b;
+}
+
+
+function subtract(a, b) {
+  return a - b;
+}
+
+
+function multiply(x, y) {
+  return x * y;
+}
+
+
+function divide(x, y) {
+  return x / y;
+}
