e9b25cb276bd644e16b0257b6929495c776e5392
Alexandre Gomes <119986066+alefgomes0@users.noreply.github.com>
2023-03-14 00:31:05 -0300

    Add a few JS functions and event listeners

--- calculator.js
+++ calculator.js
@@ -19,40 +19,67 @@ function divide(x, y) {
 
 
 function operate(num1, num2, operator) {
-  if (operator === "+") sum(num1, num2);
-  else if(operator === "-") subtract(num1, num2);
-  else if(operator === "*") multiply(num1, num2);
-  else if(operator === "/") divide(num1, num2);
+  num1 = +num1;
+  num2 = +num2;
+
+  
+  if(operator == "-") {
+    return subtract(num1, num2);
+  }
+  else if(operator == "*") {
+    return multiply(num1, num2);
+  }
+  else if(operator == "/") {
+    return divide(num1, num2);
+  }
+  else {
+    return sum(num1, num2);
+  }
 }
 
-const display = document.querySelector(".display");
+function clearDisplay() {
+  bottomNumber.textContent = "";
+  topNumber.textContent = "";
+  firstNumbers = "";
+  displayNumbers = "";
+  operation = null;
+  return 0;
+} 
+
+const clear = document.querySelector(".clear");
+const topNumber = document.querySelector(".former-number");
+const bottomNumber = document.querySelector(".current-number");
 let displayNumbers = "";
+const calculatorOperators = document.querySelectorAll(".operator");  
 const buttons = document.querySelectorAll(".number");
+const result = document.querySelector(".equals");
+
+let firstNumbers;
+let lastNumbers;
+let operation = null;
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     displayNumbers += button.textContent;
-    display.textContent = displayNumbers;
+    bottomNumber.textContent = displayNumbers;
   })
 })
 
-let operation = "";
-const calculatorOperators = document.querySelectorAll(".operator");
 calculatorOperators.forEach((sign) => {
   sign.addEventListener("click", () => {
     operation = sign.textContent;
-    display.textContent = displayNumbers + " " + operation;
+    firstNumbers = displayNumbers;
+    topNumber.textContent = `${displayNumbers} ${operation}`;
+    bottomNumber.textContent = "";
+    displayNumbers = "";
   })
+})    
+
+result.addEventListener("click", () => {
+  if (operation != null) {
+    topNumber.textContent = `${firstNumbers} ${operation} ${displayNumbers} ${"="}`;
+    bottomNumber.textContent = operate(firstNumbers, displayNumbers, operation);
+  }
 })
 
-
-let numbersDisplay = "";
-if (operation != "") {
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      numbersDisplay = button.textContent;
-      display.textContent = displayNumbers + " " + operation + " "+ numbersDisplay;
-    })
-  })  
-}
-
-
+clear.addEventListener("click", clearDisplay);
